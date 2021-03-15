842f415aa96d51c30de212e1b4c54a94415cce8d
seb <seb@sebastians-mbp.local.tld>
2021-03-15 18:23:07 -0400

    update

---
+++ TreeNode.java
@@ -0,0 +1,21 @@
+
+public class TreeNode {
+	int val;
+	TreeNode right;
+	TreeNode left;
+
+	TreeNode() {
+
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode right, TreeNode left) {
+		this.val = val;
+		this.right = right;
+		this.left = left;
+	}
+
+}
