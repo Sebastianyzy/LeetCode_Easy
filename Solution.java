83627fca466aa1d2784f25c62710c7b80d14ccf5
Sebyzy <sebyzy@gmail.com>
2021-03-24 20:14:17 -0400

    3.23 update

--- Solution.java
+++ Solution.java
@@ -164,6 +164,23 @@ public class Solution {
 //		}
 	}
 
+	public static double largestTriangleArea(int[][] points) {
+		int N = points.length;
+		double ans = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					ans = Math.max(ans, area(points[i], points[j], points[k]));
+				}
+			}
+		}
+		return ans;
+	}
+
+	private static double area(int[] P, int[] Q, int[] R) {
+		return 0.5 * Math.abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+	}
+
 	public static List<String> subdomainVisits(String[] cpdomains) {
 		Map<String, Integer> counts = new HashMap();
 		for (String domain : cpdomains) {
