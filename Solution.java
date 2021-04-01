969a47d27070721c47bdd59585d7112c88ab71e9
Sebyzy <sebyzy@gmail.com>
2021-04-01 14:03:16 -0400

    4.1 update

--- Solution.java
+++ Solution.java
@@ -164,6 +164,16 @@ public class Solution {
 //		}
 	}
 
+	public static int[][] transpose(int[][] matrix) {
+		int[][] transpose = new int[matrix[0].length][matrix.length];
+		for (int i = 0; i < transpose.length; i++) {
+			for (int j = 0; j < transpose[i].length; j++) {
+				transpose[i][j] = matrix[j][i];
+			}
+		}
+		return transpose;
+	}
+
 	public static boolean lemonadeChange(int[] bills) {
 		int five = 0;
 		int ten = 0;
