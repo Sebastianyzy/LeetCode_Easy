9058d53cf72e86f9a35815d15a05923cb0e8a964
Sebyzy <sebyzy@gmail.com>
2021-04-14 10:58:39 -0400

    4.14 update

--- Solution.java
+++ Solution.java
@@ -172,6 +172,25 @@ public class Solution {
 
 	}
 
+	public static boolean hasGroupsSizeX(int[] deck) {
+		int N = deck.length;
+		int[] count = new int[10000];
+		for (int c : deck)
+			count[c]++;
+		List<Integer> values = new ArrayList<>();
+		for (int i = 0; i < 10000; ++i)
+			if (count[i] > 0)
+				values.add(count[i]);
+		search: for (int X = 2; X <= N; ++X)
+			if (N % X == 0) {
+				for (int v : values)
+					if (v % X != 0)
+						continue search;
+				return true;
+			}
+		return false;
+	}
+
 	public static int smallestRangeI(int[] A, int K) {
 		int min = A[0], max = A[0];
 		for (int i : A) {
