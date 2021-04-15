3526c9f1eb1dac80715dddf2ade992d1c06a0ec5
Sebyzy <sebyzy@gmail.com>
2021-04-15 09:07:32 -0400

    4.15 update

--- Solution.java
+++ Solution.java
@@ -172,6 +172,23 @@ public class Solution {
 
 	}
 
+	public static String reverseOnlyLetters(String S) {
+		Stack<Character> letters = new Stack();
+		for (char c : S.toCharArray())
+			if (Character.isLetter(c))
+				letters.push(c);
+
+		StringBuilder ans = new StringBuilder();
+		for (char c : S.toCharArray()) {
+			if (Character.isLetter(c))
+				ans.append(letters.pop());
+			else
+				ans.append(c);
+		}
+
+		return ans.toString();
+	}
+
 	public static boolean hasGroupsSizeX(int[] deck) {
 		int N = deck.length;
 		int[] count = new int[10000];
