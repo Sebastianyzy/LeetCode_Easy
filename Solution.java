4948f59ff04febb4d19a31b6a58ac2b53e014eb3
Sebyzy <sebyzy@gmail.com>
2021-04-21 11:59:42 -0400

    4.21 update

--- Solution.java
+++ Solution.java
@@ -171,6 +171,20 @@ public class Solution {
 //		 }
 	}
 
+	public static int[] diStringMatch(String S) {
+		int N = S.length();
+		int lo = 0, hi = N;
+		int[] ans = new int[N + 1];
+		for (int i = 0; i < N; ++i) {
+			if (S.charAt(i) == 'I')
+				ans[i] = lo++;
+			else
+				ans[i] = hi--;
+		}
+		ans[N] = lo;
+		return ans;
+	}
+
 	public static boolean validMountainArray(int[] arr) {
 		if (arr.length < 3) {
 			return false;
