eb38c292921fc38a35abec1b08615a01b6180efb
Sebyzy <sebyzy@gmail.com>
2021-04-24 10:51:16 -0400

    4.24 update

--- Solution.java
+++ Solution.java
@@ -169,7 +169,21 @@ public class Solution {
 //				map.put(i, map.get(i + 1));
 //			}
 //		 }
+	}
 
+	public static List<Integer> addToArrayForm(int[] num, int k) {
+		int N = num.length, cur = k;
+		List<Integer> ans = new ArrayList<>();
+		int i = N;
+		while (--i >= 0 || cur > 0) {
+			if (i >= 0) {
+				cur += num[i];
+			}
+			ans.add(cur % 10);
+			cur /= 10;
+		}
+		Collections.reverse(ans);
+		return ans;
 	}
 
 	public static int[] sumEvenAfterQueries(int[] A, int[][] queries) {
