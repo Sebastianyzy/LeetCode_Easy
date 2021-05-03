1c542bd8246a894cee06d8c611cf43ba28ba8b35
Sebyzy <sebyzy@gmail.com>
2021-05-03 14:07:44 -0400

    5.3 update

--- Solution.java
+++ Solution.java
@@ -3526,6 +3526,15 @@ public class Solution {
 		return true;
 	}
 
+	public int[] numMovesStones(int a, int b, int c) {
+		int[] s = { a, b, c };
+		Arrays.sort(s);
+		if (s[2] - s[0] == 2) {
+			return new int[] { 0, 0 };
+		}
+		return new int[] { Math.min(s[1] - s[0], s[2] - s[1]) <= 2 ? 1 : 2, s[2] - s[0] - 2 };
+	}
+
 	public int findSpecialInteger(int[] arr) {
 		Map<Integer, Integer> map = new HashMap<>();
 		double ans = 0.25 * arr.length;
