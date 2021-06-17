44a99a58859a859041688dde58970ecaffdcefce
Sebyzy <sebyzy@gmail.com>
2021-06-17 10:11:43 -0400

    6.17 update

--- Solution.java
+++ Solution.java
@@ -169,6 +169,18 @@ public class Solution {
 //		 }
 	}
 
+	public static int[] createTargetArray(int[] nums, int[] index) {
+		int[] target = new int[nums.length];
+		int i = 0, k = 0;
+		while (i < index.length) {
+			for (k = target.length - 1; k > index[i]; k--)
+				target[k] = target[k - 1];
+			target[index[i]] = nums[i];
+			i++;
+		}
+		return target;
+	}
+
 	public static int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
 		int l1 = arr1.length, l2 = arr2.length, distance = l1;
 		for (int i : arr1) {
