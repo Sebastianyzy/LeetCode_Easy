6c8cd3c0afe4566045877ba4078b8c8ec126f5e7
seb <sebyzy@gmail.com>
2021-08-03 10:10:41 -0400

    8.3 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,15 @@ public class Solution {
 //			}
     }
 
+    public static int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1));
+        return Arrays.stream(nums).boxed()
+                .sorted((a, b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
+                .mapToInt(n -> n)
+                .toArray();
+    }
+
     public static char slowestKey(int[] releaseTimes, String keysPressed) {
         int maxTime = -1;
         int lastReleaseTime = 0;
