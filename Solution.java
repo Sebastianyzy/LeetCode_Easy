d55977853151cbd82327df6dcb1b344c193c5e7e
seb <sebyzy@gmail.com>
2021-08-06 08:38:23 -0400

    8.6 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,26 @@ public class Solution {
 //			}
     }
 
+    public static int[] decrypt(int[] code, int k) {
+        if (k == 0) {
+            return new int[code.length];
+        }
+        int[] prefix = new int[code.length * 2 + 1];
+        for (int i = 0; i < code.length * 2; i++) {
+            prefix[i + 1] = prefix[i] + code[i % code.length];
+        }
+        int[] ret = new int[code.length];
+        for (int i = 0; i < code.length; i++) {
+            int end = i + k;
+            if (k < 0) {
+                ret[i] = prefix[i + code.length] - prefix[end + code.length];
+            } else {
+                ret[i] = prefix[end + 1] - prefix[i + 1];
+            }
+        }
+        return ret;
+    }
+
     public static int getMaximumGenerated(int n) {
         if (n == 0) {
             return 0;
