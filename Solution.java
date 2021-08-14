409701c76ad83a2f8afc9a2927944c1fbb950120
seb <sebyzy@gmail.com>
2021-08-14 05:38:31 -0400

    8.14 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,23 @@ public class Solution {
 //			}
     }
 
+    public static int countStudents(int[] students, int[] sandwiches) {
+        int[] a = {0, 0};
+        for (int i = 0; i < students.length; i++) {
+            a[students[i]] += 1;
+        }
+        int k = 0;
+        while (k < sandwiches.length) {
+            if (a[sandwiches[k]] > 0) {
+                a[sandwiches[k]] -= 1;
+            } else {
+                break;
+            }
+            k += 1;
+        }
+        return sandwiches.length - k;
+    }
+
     public static String reformatNumber(String number) {
         number = number.replace("-", "").replace(" ", "");
         int len = number.length();
