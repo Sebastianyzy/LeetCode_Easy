3459f05f46c17fdd15c21f33fd5b9863218b0cd2
seb <sebyzy@gmail.com>
2021-08-20 13:02:57 -0400

    8.20 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,15 @@ public class Solution {
 //			}
     }
 
+    public static int[] decode(int[] encoded, int first) {
+        int[] arr = new int[encoded.length + 1];
+        arr[0] = first;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
+        }
+        return arr;
+    }
+
     public static int totalMoney(int n) {
         int finalResult = 1;
         int count = 1;
