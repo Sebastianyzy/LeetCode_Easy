d5edc4252cab3900b277784e74955f058c335dab
seb <sebyzy@gmail.com>
2021-08-24 06:14:59 -0400

    8.24 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,29 @@ public class Solution {
 //			}
     }
 
+    public static int countBalls(int lowLimit, int highLimit) {
+        int[] arr = new int[46];
+        int digit = lowLimit;
+        int id = 0;
+        int max = 1;
+        while (digit != 0) {
+            id += digit % 10;
+            digit /= 10;
+        }
+        ++arr[id];
+        for (int i = lowLimit + 1; i <= highLimit; i++) {
+            digit = i;
+            while (digit % 10 == 0) {
+                id -= 9;
+                digit /= 10;
+            }
+            ++arr[++id];
+            max = Math.max(max, arr[id]);
+        }
+        return max;
+
+    }
+
     public static String maximumTime(String time) {
         char[] ti = time.toCharArray();
         if (ti[0] == '?') {
