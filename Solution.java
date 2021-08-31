82d531a65ae0816f2fc31612a6a31e3a482b56c8
seb <sebyzy@gmail.com>
2021-08-31 12:51:28 -0400

    8.31 update

--- Solution.java
+++ Solution.java
@@ -3951,7 +3951,7 @@ public class Solution {
                 A++;
             }
         }
-        return A <= 1 && !s.contains("LLL");
+        return A <= 1 && s.indexOf("LLL") < 0;
     }
 
     public static int findLUSlength(String a, String b) {
