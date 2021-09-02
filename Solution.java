9e5cfe3647ee80c6166c5a7e866e57344faa0f4e
seb <sebyzy@gmail.com>
2021-09-02 09:20:58 -0400

    9.2 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,21 @@ public class Solution {
 //			}
     }
 
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int length = Math.min(word1.length(), word2.length());
+        for (int i = 0; i < length; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+        if (length == word1.length()) {
+            sb.append(word2.substring(length, word2.length()));
+        } else {
+            sb.append(word1.substring(length, word1.length()));
+        }
+        return sb.toString();
+    }
+
     public static String longestNiceSubstring(String s) {
         String ans = "", temp = "";
         for (int i = 0; i < s.length(); i++) {
