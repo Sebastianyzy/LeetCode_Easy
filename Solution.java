7597a8be53e4fe013f570180054a3ebd523e49e4
seb <sebyzy@gmail.com>
2021-09-07 15:43:14 -0400

    9.7 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,10 @@ public class Solution {
 //			}
     }
 
+    public static int findCenter(int[][] edges) {
+        return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ? edges[0][0] : edges[0][1];
+    }
+
     public static boolean areAlmostEqual(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
