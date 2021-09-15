b7b644f9183027fbeb13714028933592c8e9c665
seb <sebyzy@gmail.com>
2021-09-15 16:19:31 -0400

    9.15 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,18 @@ public class Solution {
 //			}
     }
 
+    public static int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                int temp = nums[i - 1] - nums[i] + 1;
+                count += temp;
+                nums[i] += temp;
+            }
+        }
+        return count;
+    }
+
     public static int badSensor(int[] sensor1, int[] sensor2) {
         int i = 0;
         while (i < sensor1.length) {
