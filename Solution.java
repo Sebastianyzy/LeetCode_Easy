f10f8f72b998ff61507c32c1a2be8884736af86e
seb <sebyzy@gmail.com>
2021-09-26 13:47:14 -0400

    9.26 update

--- Solution.java
+++ Solution.java
@@ -165,6 +165,32 @@ public class Solution {
 //			}
     }
 
+    public static boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        int[] res[] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = mat[n - 1 - j][i];
+            }
+        }
+        int[] res2[] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res2[i][j] = res[n - 1 - j][i];
+            }
+        }
+        int[] res3[] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res3[i][j] = res2[n - 1 - j][i];
+            }
+        }
+        if (Arrays.deepEquals(target, res) || Arrays.deepEquals(target, res2) || Arrays.deepEquals(target, res3) || Arrays.deepEquals(target, mat)) {
+            return true;
+        }
+        return false;
+    }
+
     public static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
         HashMap<Character, Integer> map = new HashMap<>();
         int i = 0;
