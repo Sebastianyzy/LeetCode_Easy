1f8873c7961a9d56c69d4a93d609df8dbd238638
seb <sebyzy@gmail.com>
2021-10-14 14:46:45 -0400

    10.14 update

--- Solution.java
+++ Solution.java
@@ -183,6 +183,7 @@ public class Solution {
                 if (i == words.length - 1 && !s.equals("")) {
                     return false;
                 }
+                continue;
             } else {
                 return false;
             }
