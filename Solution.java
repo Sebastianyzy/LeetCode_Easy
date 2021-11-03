5cd2ff4ea8a49864692f6b176de89077148ce641
seb <sebyzy@gmail.com>
2021-11-03 18:26:42 -0400

    11.3 update

--- Solution.java
+++ Solution.java
@@ -1729,8 +1729,7 @@ public class Solution {
     }
 
     public static int minStartValue(int[] nums) {
-        int sum = 0;
-        int minSum = 0;
+        int sum = 0, minSum = 0;
         for (int num : nums) {
             sum += num;
             minSum = Math.min(sum, minSum);
