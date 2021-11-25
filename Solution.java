9d8f530d3b72ef48b95c9182ee6f204a967dfeb1
seb <sebyzy@gmail.com>
2021-11-25 14:26:55 -0500

    11.25 update

--- Solution.java
+++ Solution.java
@@ -5332,12 +5332,10 @@ public class Solution {
         for (int j = 0; j < n2; ++j) {
             R[j] = arr[mid + 1 + j];
         }
-
         /* Merge the temp arrays */
         // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
         // Initial index of merged subarry array
-        int k = start;
+        int i = 0, j = 0, k = start;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
                 arr[k] = L[i];
@@ -5349,14 +5347,12 @@ public class Solution {
             }
             k++;
         }
-
         /* Copy remaining elements of L[] if any */
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-
         /* Copy remaining elements of R[] if any */
         while (j < n2) {
             arr[k] = R[j];
