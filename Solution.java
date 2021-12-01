916220368921911850cfdb5771bc68ad642a4d7d
seb <sebyzy@gmail.com>
2021-12-01 14:37:02 -0500

    12.1 update

--- Solution.java
+++ Solution.java
@@ -6013,8 +6013,7 @@ public class Solution {
     }
 
     public static int maxSubArray(int[] nums) {
-        int max = nums[0];
-        int cur = nums[0];
+        int max = nums[0], cur = nums[0];
         for (int i = 1; i < nums.length; i++) {
             cur = Math.max(nums[i], cur + nums[i]);
             max = Math.max(max, cur);
