eaf8423592e12a86542057a017052ae740e4bafa
seb <sebyzy@gmail.com>
2021-12-16 13:17:11 -0500

    12.16 update

--- Solution.java
+++ Solution.java
@@ -166,25 +166,22 @@ public class Solution{
     }
 
     public static String reversePrefix(String word, char ch){
-        String replace = "";
-        if(!word.contains(Character.toString(ch))){
-            return word;
-        }
+        StringBuilder replace = new StringBuilder();
         for(int i = 0; i < word.length(); i++){
             if(word.charAt(i) == ch){
                 for(int j = i; j >= 0; j--){
-                    replace += Character.toString(word.charAt(j));
+                    replace.append(word.charAt(j));
                 }
                 if(i == word.length() - 1){
-                    return replace;
+                    return replace.toString();
                 }
                 for(int k = i + 1; k < word.length(); k++){
-                    replace += Character.toString(word.charAt(k));
+                    replace.append(word.charAt(k));
                 }
                 break;
             }
         }
-        return replace;
+        return replace.length() == 0 ? word : replace.toString();
     }
 
     public static int minimumMoves(String s){
